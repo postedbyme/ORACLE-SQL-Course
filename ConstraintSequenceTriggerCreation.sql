0035e8b9d0962cf568f4dedf94a82d1408393b87
postedbyme <myofficialemail1@gmail.com>
2020-07-21 00:56:10 +0200

    Update ConstraintSequenceTriggerCreation.sql

--- ConstraintSequenceTriggerCreation.sql
+++ ConstraintSequenceTriggerCreation.sql
@@ -1,26 +1,33 @@
-CREATE TABLE bankStatement 
-(
-  trans_id     NUMBER(3)    NOT NULL,
-  dated        DATE         NOT NULL,
-  Details      VARCHAR2(60) NOT NULL,
-  paid_in      NUMBER(8,2),
-  paid_out     NUMBER(8,2)
-  );
+CREATE TABLE bankstatement (
+	trans_id NUMBER(3) NOT NULL,
+	dated DATE NOT NULL,
+	details VARCHAR2(60) NOT NULL,
+	paid_in NUMBER(8, 2),
+	paid_out NUMBER(8, 2)
+);
 
-ALTER TABLE bankStatement ADD (
-  CONSTRAINT bank_pk PRIMARY KEY (ID));
+ALTER TABLE bankstatement ADD (
+	CONSTRAINT bank_pk PRIMARY KEY ( id )
+);
 
 CREATE SEQUENCE bank_seq START WITH 100;
 
-CREATE OR REPLACE TRIGGER bank_bir 
-BEFORE INSERT ON bankStatement 
-FOR EACH ROW
-
+CREATE OR REPLACE TRIGGER bank_bir BEFORE
+	INSERT ON bankstatement
+	FOR EACH ROW
 BEGIN
-  SELECT bank_seq.NEXTVAL
-  INTO   :new.id
-  FROM   dual;
+	SELECT
+		bank_seq.NEXTVAL
+	INTO:new.id
+	FROM
+		dual;
+
 END;
 /
 
-SELECT * FROM bankStatement ORDER BY DATED;
+SELECT
+	*
+FROM
+	bankstatement
+ORDER BY
+	dated;
